bd49d0acfc108be7268c8821e5519fb5deaf693b
Nwaokocha Victor <nvonweb@outlook.com>
2023-03-16 03:29:37 -0400

    create dir

--- single_web_server/single_web_server.tf
+++ single_web_server/single_web_server.tf
@@ -1,4 +1,3 @@
-/*
 provider "aws" {
     region = "us-east-2"
 }
@@ -26,4 +25,4 @@ resource "aws_security_group" "instance" {
         to_port   = 8080
         cidr_blocks = ["0.0.0.0/0"]
     }
-}*/
+}
